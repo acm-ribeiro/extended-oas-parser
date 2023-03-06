d8150f5e0571dcf48207e1601ee8f2690764cd5c
acm.ribeiro <acm.ribeiro@campus.fct.unl.pt>
2023-03-06 11:32:07 +0000

    simplify tests

--- src/test/java/TestDomain.java
+++ src/test/java/TestDomain.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-import parser_domain.APIProperty;
 import parser_domain.Endpoint;
 import parser_domain.URLParameter;
 
@@ -10,61 +9,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDomain {
 
-    String SPEC_FILE = "/Users/acm.ribeiro/Desktop/run-specs/apostl-specs/tournaments-apostl-spec-extended.json";
-
     @Test
     void endpoint_test1() {
-        // Path parameters
         List<URLParameter> path = new ArrayList<>();
-        APIProperty prop = new APIProperty("resourceId", "string", "", "", "", -99, 99, false, true, false);
-        List<APIProperty> schema = new ArrayList<>();
-        schema.add(prop);
-        URLParameter par = new URLParameter("path", "resourceId", true, schema);
-
-        // Empty query parameters
         List<URLParameter> query = new ArrayList<>();
-
         Endpoint e = new Endpoint("/resources/{resourceId}", path, query);
 
-        // Value for the path parameter
-        String val = "123";
-        e.putPathValue(par.getName(), val);
+        // updating value
+        e.putPathValue("resourceId", "123");
 
         String expected = "/resources/123";
         String actual = e.toString();
-
         assertEquals(expected, actual);
     }
 
     @Test
     void endpoint_test2 () {
-        // Path parameters
         List<URLParameter> path = new ArrayList<>();
-        APIProperty prop = new APIProperty("resourceId", "string", "", "", "", -99, 99, false, true, false);
-        List<APIProperty> schema = new ArrayList<>();
-        schema.add(prop);
-        URLParameter par = new URLParameter("path", "resourceId", true, schema);
-
-        // Empty query parameters
         List<URLParameter> query = new ArrayList<>();
-        APIProperty q_prop = new APIProperty("sort", "string", "", "", "", -99, 99, false, false, false);
-        List<APIProperty> q_schema = new ArrayList<>();
-        q_schema.add(q_prop);
-        URLParameter q_par = new URLParameter("path", "sort", false, q_schema);
-
         Endpoint e = new Endpoint("/resources/{resourceId}", path, query);
 
-        // Value for the path parameter
-        String val = "123";
-        e.putPathValue(par.getName(), val);
-
-        // Value for query parameter
-        String q_val = "name";
+        // updating values
+        e.putPathValue("resourceId", "123");
         e.putQueryValue("sort", "name");
 
         String expected = "/resources/123?sort=name";
         String actual = e.toString();
-
         assertEquals(expected, actual);
     }
 }
